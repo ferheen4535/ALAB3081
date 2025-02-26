4c54dd8da7ba7b8491440e8bd342ae2dccb3d6fb
ferheen4535 <ferheen.syed@gmail.com>
2025-02-25 19:41:23 -0500

    partone

---
+++ index.js
@@ -0,0 +1,11 @@
+// The initial numbers that must be verified.
+const n1 = 9;
+const n2 = 23;
+const n3 = 12;
+const n4 = 6;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+console.log(isSum50)
