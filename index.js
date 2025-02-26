eecc45740a826e6187322a04937335673101b2ba
ferheen4535 <ferheen.syed@gmail.com>
2025-02-25 20:51:28 -0500

    commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const n1 = 9;
-const n2 = 23;
-const n3 = 12;
-const n4 = 6;
+const n1 = 15;
+const n2 = 5;
+const n3 = 20;
+const n4 = 10;
 
-console.log (isSum50);
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+
+const isSum50 = (n1 + n2 + n3 + n4) === 50;
+
+console.log("Sum is 50:", isSum50);
